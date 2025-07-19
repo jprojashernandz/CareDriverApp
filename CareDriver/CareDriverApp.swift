46978538164cea77f8d6c6e9d26d247afbaa0429
JP Rojas Hernandez <jprojashernandez@gmail.com>
2025-07-19 09:38:47 -0500

    Initial Commit

---
+++ CareDriver/CareDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareDriverApp.swift
+//  CareDriver
+//
+//  Created by JP Rojas Hernández on 19/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CareDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareDriver/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CareDriver
+//
+//  Created by JP Rojas Hernández on 19/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
